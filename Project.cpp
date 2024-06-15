90ea099660de8abf84aa07a8ab2c11cb62ec064f
bijudd <bijud@mcmaster.ca>
2024-06-15 17:18:03 -0400

    2A from vid

--- Project.cpp
+++ Project.cpp
@@ -142,3 +142,4 @@ void CleanUp(void)
 
 
 
+
--- objPosArrayList_TestSuite/objPosArrayList.cpp
+++ objPosArrayList_TestSuite/objPosArrayList.cpp
@@ -5,21 +5,35 @@
 
 objPosArrayList::objPosArrayList()
 {
-
+        aList = new objPos[ARRAY_MAX_CAP]; // 2500 elements on the heap 
+        listSize = 0; // initial list
+        arrayCapacity = ARRAY_MAX_CAP; // array capacity at max
 }
 
 objPosArrayList::~objPosArrayList()
 {
-
+    delete[] aList; // this is becuase aList is an array
 }
 
 int objPosArrayList::getSize()
 {
-
+return listSize;
 }
 
 void objPosArrayList::insertHead(objPos thisPos)
 {
+
+    if (listSize != arrayCapacity){
+
+    
+
+        for (int i = listSize; i >0; i--){
+            aList[i].setObjPos(aList[i-1]);
+        }
+
+        aList[0].setObjPos(thisPos); // this shuffles all elements towards the tail
+        listSize++;
+} 
 
 }
 
@@ -40,12 +54,12 @@ void objPosArrayList::removeTail()
 
 void objPosArrayList::getHeadElement(objPos &returnPos)
 {
-
+returnPos.setObjPos(aList[0]);
 }
 
 void objPosArrayList::getTailElement(objPos &returnPos)
 {
-
+returnPos.setObjPos(aList[listSize-1]);
 }
 
 void objPosArrayList::getElement(objPos &returnPos, int index)
