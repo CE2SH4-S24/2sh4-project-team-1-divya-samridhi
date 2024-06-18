6642cbf0afe209c0962b06abddd48b4d745bdfcc
bijudd <bijud@mcmaster.ca>
2024-06-18 19:16:22 -0400

    test cases

--- objPosArrayList_TestSuite/Test.cpp
+++ objPosArrayList_TestSuite/Test.cpp
@@ -531,6 +531,244 @@ void testRemoveTail_5Element()
 
 
 
+//2a
+
+void testInsertTailMultipleElements()
+{
+	objPos currentPos;
+	objPos samplePos1(2, 5, 'a');
+	objPos samplePos2(3, 6, 'b');  
+	objPos samplePos3(4, 7, 'c');  
+
+	// Insert 3 sample elements at the tail
+	objPosArrayList thisList;
+	thisList.insertTail(samplePos1);
+	thisList.insertTail(samplePos2);
+	thisList.insertTail(samplePos3);
+
+	int expectedSize = 3;
+	int actualSize = thisList.getSize();
+		
+	// First check the list size is 3
+	if(expectedSize != actualSize)
+	{
+		cout << "[FAILED] Test Insert Tail Multiple Elements Failed - wrong size" << endl;
+		failCount++;
+		passed = false;	
+		return;
+	}
+
+	bool expectedCheck = true;
+	bool actualCheck;
+
+	// Check whether the head element is the first inserted element
+	thisList.getHeadElement(currentPos);
+	actualCheck = samplePos1.isPosEqual(&currentPos);
+
+	if(expectedCheck != actualCheck)
+	{
+		cout << "[FAILED] Test Insert Tail Multiple Elements Failed - head wrong" << endl;
+		failCount++;
+		passed = false;	
+		return;
+	}
+
+	// Check whether the tail element is the last inserted element
+	thisList.getTailElement(currentPos);
+	actualCheck = samplePos3.isPosEqual(&currentPos);
+
+	if(expectedCheck != actualCheck)
+	{
+		cout << "[FAILED] Test Insert Tail Multiple Elements Failed - tail wrong" << endl;
+		failCount++;
+		passed = false;	
+		return;
+	}
+
+	// Check whether the elements are in the correct order
+	thisList.getElement(currentPos, 0);
+	actualCheck = samplePos1.isPosEqual(&currentPos);
+	if(expectedCheck != actualCheck)
+	{
+		cout << "[FAILED] Test Insert Tail Multiple Elements Failed - element 0 wrong" << endl;
+		failCount++;
+		passed = false;	
+		return;
+	}
+
+	thisList.getElement(currentPos, 1);
+	actualCheck = samplePos2.isPosEqual(&currentPos);
+	if(expectedCheck != actualCheck)
+	{
+		cout << "[FAILED] Test Insert Tail Multiple Elements Failed - element 1 wrong" << endl;
+		failCount++;
+		passed = false;	
+		return;
+	}
+
+	thisList.getElement(currentPos, 2);
+	actualCheck = samplePos3.isPosEqual(&currentPos);
+	if(expectedCheck != actualCheck)
+	{
+		cout << "[FAILED] Test Insert Tail Multiple Elements Failed - element 2 wrong" << endl;
+		failCount++;
+		passed = false;	
+		return;
+	}
+
+	cout << "[OK] Test Insert Tail Multiple Elements Passed" << endl;
+	passCount++;	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void testInsertTailElement()
+{
+    objPos currentPos;
+    objPos bodyPos(2, 5, 'a');
+    objPos tailPos(3, 3, 'm');
+
+    // Insert 4 body elements, then 1 unique tail element
+    objPosArrayList thisList;
+    thisList.insertTail(bodyPos);
+    thisList.insertTail(bodyPos);
+    thisList.insertTail(bodyPos);
+    thisList.insertTail(bodyPos);
+    thisList.insertTail(tailPos);
+
+    int expectedSize = 5;
+    int actualSize = thisList.getSize();
+
+    // Confirm the list size is now 5
+    if(expectedSize != actualSize)
+    {
+        cout << "[FAILED] Test Insert Tail 5 Element Failed - size wrong" << endl;
+        failCount++;
+        passed = false;
+        return;
+    }
+
+    bool expectedCheck = true;
+    bool actualCheck;
+
+    // Check the tail element is the unique element
+    thisList.getTailElement(currentPos);
+    actualCheck = tailPos.isPosEqual(&currentPos);
+
+    if(expectedCheck != actualCheck)
+    {
+        cout << "[FAILED] Test Insert Tail 5 Element Failed - tail wrong" << endl;
+        failCount++;
+        passed = false;
+        return;
+    }
+
+    // Next, check the body elements at index 0, 1, 2, and 3.
+    for(int i = 0; i < actualSize - 1; i++)
+    {
+        thisList.getElement(currentPos, i);
+        actualCheck = bodyPos.isPosEqual(&currentPos);
+
+        if(expectedCheck != actualCheck)
+        {
+            cout << "[FAILED] Test Insert Tail 5 Element Failed - body wrong" << endl;
+            failCount++;
+            passed = false;
+            return;
+        }
+    }
+
+    // Check the head element is the body element
+    thisList.getHeadElement(currentPos);
+    actualCheck = bodyPos.isPosEqual(&currentPos);
+
+    if(expectedCheck != actualCheck)
+    {
+        cout << "[FAILED] Test Insert Tail 5 Element Failed - head wrong" << endl;
+        failCount++;
+        passed = false;
+        return;
+    }
+
+    cout << "[OK] Test Insert Tail 5 Element Passed" << endl;
+    passCount++;
+}
+void testInsertBeyondCapacity()
+{
+    const int initialCapacity = 200;
+    const int numElements = initialCapacity + 1; // One more than the initial capacity
+
+    objPos currentPos;
+    objPos testPos(1, 1, 'a');
+
+    // Create the list with default capacity
+    objPosArrayList thisList;
+
+    // Insert elements beyond the initial capacity
+    for (int i = 0; i < numElements; ++i)
+    {
+        thisList.insertHead(testPos);
+    }
+
+    int expectedSize = numElements;
+    int actualSize = thisList.getSize();
+
+    // Confirm the list size is as expected
+    if(expectedSize != actualSize)
+    {
+        cout << "[FAILED] Test Insert Beyond Capacity Failed - size wrong" << endl;
+        failCount++;
+        passed = false;
+        return;
+    }
+
+    bool expectedCheck = true;
+    bool actualCheck;
+
+    // Check the head element is the same as the test element
+    thisList.getHeadElement(currentPos);
+    actualCheck = testPos.isPosEqual(&currentPos);
+
+    if(expectedCheck != actualCheck)
+    {
+        cout << "[FAILED] Test Insert Beyond Capacity Failed - head wrong" << endl;
+        failCount++;
+        passed = false;
+        return;
+    }
+
+    // Check the tail element is the same as the test element
+    thisList.getTailElement(currentPos);
+    actualCheck = testPos.isPosEqual(&currentPos);
+
+    if(expectedCheck != actualCheck)
+    {
+        cout << "[FAILED] Test Insert Beyond Capacity Failed - tail wrong" << endl;
+        failCount++;
+        passed = false;
+        return;
+    }
+
+    cout << "[OK] Test Insert Beyond Capacity Passed" << endl;
+    passCount++;
+}
+
+
+
+
+
+
+
 int main(int argc, char const *argv[]) {
 	
 	
@@ -543,9 +781,10 @@ int main(int argc, char const *argv[]) {
 	testRemoveHead_5Element();
 	testRemoveTail_1Element();
 	testRemoveTail_5Element();
+	testInsertTailElement();
 	
 
-
+testInsertTailMultipleElements();
 
 	//TODO add your test here
 
