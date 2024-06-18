acf6c7482f07b3ab615cf03d16e816e6e6f4ebd2
bijudd <bijud@mcmaster.ca>
2024-06-18 07:21:53 -0400

    iteration 3

--- Food.cpp
+++ Food.cpp
@@ -19,19 +19,15 @@ void Food::generateFood(objPos blockOff) {
    
     
     // Generate food at a random position within the board size
-     foodPos.x = rand() % (boardSizeX -2);
-    foodPos.y = rand() % (boardSizeY -2);
-        
-    foodPos.symbol = 'o';
+  foodPos.setObjPos(rand() % (boardSizeX - 2), rand() % (boardSizeY - 2), 'o');
+
 
 
  // Check if generated position is already taken
-        while ((blockOff.x == foodPos.x  && blockOff.y == foodPos.y)) {
+        while (blockOff.isPosEqual(&foodPos)) {
             // Generate new coordinates until a free one is found
-            foodPos.x = rand() % (boardSizeX -2);
-            foodPos.y = rand() % (boardSizeY -2);
-              foodPos.symbol = 'o';
-        
+            foodPos.setObjPos(rand() % (boardSizeX - 2), rand() % (boardSizeY - 2), 'o');
+    
         }
 
 
