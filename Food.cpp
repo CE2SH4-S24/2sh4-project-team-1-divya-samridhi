92d0c153d467fc32ffeb087abf16cd106fba036e
Divya Biju <169090185+bijudd@users.noreply.github.com>
2024-06-27 10:24:36 -0400

    Add files via upload

--- Food.cpp
+++ Food.cpp
@@ -4,7 +4,7 @@
 
 #define boardSizeX 20
 #define boardSizeY 10 // initial board size
-
+using namespace std;
 
 Food::Food(){
     srand(time(NULL)); // seeding random number generator
@@ -17,39 +17,56 @@ Food::~Food() {
     delete foodBucket;
 }
 
-
-// random food generation
 void Food::generateFood(objPos blockOff) {
+   foodBucket->clear();
+   numSpecialFoods = 0;
+   int generatedFoods = 0;
 
-    foodBucket->clear(); // Clear current food items
-    numSpecialFoods = 0; // Reset special food count
+
+   while (generatedFoods < 5) {
+       int xPos, yPos;
+       bool overlap = false;
+
+       do {
+           xPos = rand() % (boardSizeX-3); // THIS IS THE BIGGEST MISTAKE WE MADE it should not be outside the boarder!!
+           yPos = rand() % (boardSizeY-3); // By making it -3, it ensures that the food doesn't overlap with the boarder coordinates and so the 5 food items are clearly visible
+          
+           overlap = false; // Reset overlap for each new position
+          
+           // Check if the generated position overlaps with existing food items NEXT BIGGEST MISTAKE WE MADE
+           for (int j = 0; j < foodBucket->getSize(); j++) {
+               objPos currentPos;
+               foodBucket->getElement(currentPos, j);
+               if (currentPos.getX() == xPos && currentPos.getY() == yPos) {
+                   overlap = true; // There is an overlap
+                   break; // Exit the loop, this is how overlap is being handled within food items
+               }
+           }
 
 
+           // Check if the generated position is overlapping with the player 
+           if (xPos == blockOff.getX() && yPos == blockOff.getY()) {
+               overlap = true; // There is an overlap with the player
+           }
 
-    for (int i = 0; i < 5; i++) {
-        int xPos, yPos;
-        bool posIsBlocked = true;
+       } while (overlap); // Repeat until no overlap is found
 
-        while (posIsBlocked) {
-            xPos = rand() % boardSizeX;
-            yPos = rand() % boardSizeY;
 
-            if (xPos != blockOff.getX() || yPos != blockOff.getY()) {
-                posIsBlocked = false;
-            }
-        }
-
-        char symbol = 'o'; // Default food symbol
-        if (numSpecialFoods < maxSpecialFoods && rand() % 5 < 2) {
-            symbol = '$'; // Special food symbol
-            ++numSpecialFoods;
-        }
-
-        objPos foodPos(xPos, yPos, symbol);
-        foodBucket->insertTail(foodPos); // Add the food item to the foodBucket
-    }
     
+       char symbol = 'o';
+       if (numSpecialFoods < maxSpecialFoods && rand() % 5 < 2) {
+           symbol = '$'; // Special food 
+           numSpecialFoods++;
+       }
+
+
+       objPos foodPos(xPos, yPos, symbol);
+       foodBucket->insertTail(foodPos); // Insert the new food item into the bucket
+
+       generatedFoods++;
+   }
 }
+
 
 void Food::getFoodPos(objPos& returnPos) {
     foodBucket->getHeadElement(returnPos); // Example: Get the first food item in the bucket
